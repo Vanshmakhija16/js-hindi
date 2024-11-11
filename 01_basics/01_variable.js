e79e17dcd02d030a38811f7859ce166ecfd33994
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2024-11-11 00:06:53 +0000

    Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountID=164
+let accountEmail="vanshmakhija@gmail.com"
+var accountPassword="123450"
+accountCity="Tijara"
+let accountState;
+
+//accountID = 2 //not allowed
+
+accountEmail = "vm@k.com"
+accountPassword = "6562"
+accountCity="Alwar"
+
+console.log(accountID);
+
+/*
+Prefer not to use var
+because of issuein block scope
+*/
