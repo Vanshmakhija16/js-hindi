29170047f17041fffc281b7562aeae38fc567ace
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2024-11-12 07:56:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score ="33"
+
+
+console.log(typeof score)
+
+let valueInNumber =Number(score)
+console.log((typeof(valueInNumber)));
+console.log(valueInNumber);
+
+
+/* "33" => 33
+    "33abc=>NaN"
+    true=> 1
+    false=> 0
+    */
+
+    let issLoggedIN=1
+
+
+    let boolIsLoggedIn =Boolean(issLoggedIN)
+    console.log(boolIsLoggedIn);
+    
+    //1= true;0=false
+    //""=false
+    //"vansh"=true
+
+    let a= 33
+
+    let stringnum=String(a)
+    console.log(typeof stringnum);
+    
