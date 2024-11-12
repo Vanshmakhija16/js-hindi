728f69e213225f5f48448ce1b4902f6928d27ece
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2024-11-12 08:11:40 +0000

    prefix postfix

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,3 +29,40 @@ console.log(valueInNumber);
     let stringnum=String(a)
     console.log(typeof stringnum);
     
+
+    //************************operations********************/
+
+      let  value =3
+      let negvalue= -value
+      console.log(negvalue);
+      
+
+      console.log(2+2);
+      console.log(2-2);
+      console.log(2*2);
+      console.log(2**2);
+      console.log(2/2);
+      console.log(2%2);
+
+   let str1="hello"
+   let str2=" vansh"
+
+   let str3 =str1+str2
+   console.log(str3);
+
+   console.log("1" + 3);
+   console.log(1 +"3");
+   console.log("1"+2+2);
+   console.log(3+2+"2");
+
+
+   console.log(+true);
+   console.log(+"");
+
+let num1=num2=num3=2+2
+console.log(num1,num2,num3);
+
+let gameCounter=100;
+++gameCounter;
+gameCounter++;
+console.log();
